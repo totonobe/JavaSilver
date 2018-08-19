88c30f58ee8c587c6b63d4c148093417cb1ab571
nobepan <nobepan@gmail.com>
2018-08-19 15:00:01 +0900

    test8_02
    test

--- src/test8_02/Main.java
+++ src/test8_02/Main.java
@@ -8,6 +8,6 @@ public class Main {
 		System.out.println(str);
 	}
 	private static void hello(String str) {
-		System.out.println(str.replaceAll("hoge", "hello"));
+		System.out.println(str.replaceAll("hoge", "hello"));// test
 	}
 }
