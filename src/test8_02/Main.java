4ee1caa49604e8711061da78e229a180755566d4
nobepan <nobepan@gmail.com>
2018-08-19 14:57:41 +0900

    Initial commit

---
+++ src/test8_02/Main.java
@@ -0,0 +1,13 @@
+package test8_02;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String str = "hoge, world.";
+		hello(str);
+		System.out.println(str);
+	}
+	private static void hello(String str) {
+		System.out.println(str.replaceAll("hoge", "hello"));
+	}
+}
