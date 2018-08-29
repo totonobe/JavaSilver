d12f5e1627d245986fad2192205a51a3d617c4a7
nobe <toru.ichinobe@careritz.co.jp>
2018-08-29 18:21:45 +0900

    いろいろな初期化

---
+++ src/test10/test000.java
@@ -0,0 +1,21 @@
+package test10;
+
+public class test000 {
+	public static void main(String[] args) {
+	//初期化に自信がなくなったので復習している。
+	String str = "String";
+		System.out.println(str);
+	boolean bool1 = true;
+		System.out.println("boolean bool1 = true :" + bool1);
+	boolean bool2 = false;
+		System.out.println("boolean bool2 = false :" + bool2);
+	boolean bool3 = (1 + 3 == 2 + 0);
+		System.out.println("boolean bool3 = (1 + 3 == 2 + 0) :" + bool3);
+	boolean bool4 = ("String" == str);
+		System.out.println(bool4);
+	char chr1 = 97;
+		System.out.println(chr1);
+	char chr2 = 123;
+		System.out.println(chr2);
+	}
+}
